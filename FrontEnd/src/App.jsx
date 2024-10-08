ec2ba210f5c10e42cd77511d7e6f70e6bcd42e3d
Md Faisal Raza Khan <irkfaisal@gmail.com>
2024-10-09 00:35:00 +0530

    initial gradient background for hero page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,9 +4,17 @@ function App() {
 
   return (
     <>
-      <p className="">
-        AIResumeMailer
-      </p>
+      {/* <div className="bg-dark-gradient h-screen w-full flex items-start justify-center">
+        <h1 className="text-white text-4xl font-bold drop-shadow-lg">
+          <span className="bg-gradient-to-r from-cyan-300 via-teal-400 to-blue-500 text-transparent bg-clip-text">
+            Welcome to Your AI App
+          </span>
+        </h1>
+      </div> */}
+      <div className="gradient__bg h-screen w-full flex items-center justify-center">
+        <h1 className="gradient__text text-4xl font-bold">Welcome to Your AI App</h1>
+      </div>
+
     </>
   )
 }
--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // backgroundImage: {
+      //   'dark-gradient': 'linear-gradient(10.81deg, #0c1b3a -43.27%, #0a1733 -21.24%, #0a1130 12.19%, #090f29 29.82%, #070b23 51.94%, #040615 90.29%)',
+      // },
+      backgroundImage: {
+        'dark-gradient': 'linear-gradient(360.81deg, #0b162e -43.27%, #090e23 -21.24%, #080c20 5%, #07081d 20%, #050618 35%, #040515 51.94%, #02030e 70%, #000000 100%)',
+      },
+      
+    },
   },
   plugins: [],
 }
