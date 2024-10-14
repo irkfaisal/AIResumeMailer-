e49a26fd9fed3135e45033146fdde8fcc61e1258
Md Faisal Raza Khan <irkfaisal@gmail.com>
2024-10-15 00:51:33 +0530

    hero banner page design

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,17 +1,30 @@
 import './App.css'
+import { img4 } from './assets'
 
 function App() {
 
   return (
     <>
-      {/* <div className="bg-dark-gradient h-screen w-full flex items-start justify-center">
-        <h1 className="text-white text-4xl font-bold drop-shadow-lg">
-          <span className="bg-gradient-to-r from-cyan-300 via-teal-400 to-blue-500 text-transparent bg-clip-text">
-            Welcome to Your AI App
-          </span>
-        </h1>
-      </div> */}
-      <div className="gradient__bg h-screen w-full flex items-center justify-center">
+      <div className="gradient__bg w-screen flex flex-wrap items-center justify-center" >
+        <div
+          className="h-[500px] w-[500px] bg-transparent flex justify-center items-center"
+          style={{
+            border: "2px solid white",
+            borderRadius: "50%",
+            boxShadow: "0 0 20px 10px rgba(255, 255, 255, 0.13)",
+          }}
+        >
+          <div
+            className="h-[400px] w-[400px] bg-transparent flex justify-center items-center"
+            style={{
+              border: "2px solid white",
+              borderRadius: "50%",
+              boxShadow: "0 0 20px 10px rgba(255, 255, 255, 0.5)",
+            }}
+          >
+            <img src={img4} className="h-60 w-60" alt="mail-logo" />
+          </div>
+        </div>
         <h1 className="gradient__text text-4xl font-bold">Welcome to Your AI App</h1>
       </div>
 
---
+++ FrontEnd/src/assets/index.js
@@ -0,0 +1,9 @@
+import img1 from './aihandimg.png'
+import img2 from './aiLogo.png'
+import img3 from './aiFace.png'
+import img4 from './aimailimg2.png'
+import img5 from './ai-mail.png'
+
+export {
+    img1, img2, img3, img4, img5
+}
