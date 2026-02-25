96c6e0e2ec78e266f4d2fb235dda7386b8db6f15
Faisal <irkfaisal@gmail.com>
2026-02-25 12:34:09 +0530

    added some anaylistics function of vercel in frontend

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -4,16 +4,16 @@ import App from './App.jsx';
 import { Toaster } from 'sonner';
 import './index.css';
 import { ContextProvider } from './context/ContextProvider';
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContextProvider>
       <Toaster richColors position="top-center" />
       <App />
-      <SpeedInsights />
-      <Analytics />
+      {/* <SpeedInsights />
+      <Analytics /> */}
     </ContextProvider>
   </StrictMode>
 );
